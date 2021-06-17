493a8c678009ec6919705925994d6ec39acd6c7a
HoracioGutierrez <horacio.estevez@gmail.com>
2021-06-17 13:53:08 -0300

    Add custom App layout and create main Header

---
+++ components/layout/Header.js
@@ -0,0 +1,9 @@
+const Header = () => 
+    <header>
+        <h1>Mi Portfolio</h1>
+        <nav>
+            <a href="#">Link</a>
+        </nav>
+    </header>
+
+export default Header
---
+++ pages/_app.js
@@ -0,0 +1,12 @@
+import Header from "../components/layout/Header"
+
+function MyApp ({Component,pageProps}) {
+    return (
+        <>
+        <Header/>
+        <Component {...pageProps}/>
+        </>
+    )
+}
+
+export default MyApp
